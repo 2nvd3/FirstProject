25a87b4627a44cc4090b1eb78e656a53689c7d7f
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-11 23:46:47 +0700

    Create Enemy.cpp

---
+++ CPP/Enemy.cpp
@@ -0,0 +1,57 @@
+#include "../Header/Enemy.h"
+
+void Enemy::initTex()
+{
+	this->enemyTex.loadFromFile("Image/Character/enemy.png");
+}
+
+void Enemy::initVari()
+{
+	this->PointCount = rand() % 100 + 25;
+	this->type = 0;
+	this->speed = rand()%5+1;
+	this->hpMax = static_cast<float>(this->PointCount);
+	this->hp = this->hpMax;
+	this->damage = this->PointCount;
+	this->points = this->PointCount;
+}
+
+void Enemy::initSprite()
+{
+	this->enemySprite.setTexture(this->enemyTex);
+}
+
+const int& Enemy::getDamage() const
+{
+	// TODO: insert return statement here
+	return this->damage;
+}
+
+const int& Enemy::getPoints() const
+{
+	// TODO: insert return statement here
+	return this->points;
+}
+
+const sf::FloatRect Enemy::getBounds() const
+{
+	return this->enemySprite.getGlobalBounds();
+}
+
+void Enemy::update()
+{
+	this->enemySprite.move(-this->speed, 0);
+}
+
+void Enemy::render(sf::RenderTarget* target)
+{
+	target->draw(this->enemySprite);
+}
+
+Enemy::Enemy(float pos_x, float pos_y)
+{
+	this->initTex();
+	this->initSprite();
+	this->initVari();
+	this->enemySprite.setPosition(pos_x, pos_y);
+}
