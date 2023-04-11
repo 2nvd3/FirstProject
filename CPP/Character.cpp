1b3d59e3941e4b98aa87e98083d45679e3e50ece
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-11 23:45:03 +0700

    Create Character.cpp

---
+++ CPP/Character.cpp
@@ -0,0 +1,79 @@
+#include "../Header/Character.h"
+
+void Character::initTexture()
+{
+	this->texture.loadFromFile("Image/Character/bird.png");
+}
+
+void Character::initSprite()
+{
+	this->sprite.setTexture(this->texture);
+	this->sprite.scale(1.3f,1.3f);
+}
+
+const sf::FloatRect Character::getBounds() const
+{
+	return this->sprite.getGlobalBounds();
+}
+
+const int& Character::getHp() const
+{
+	// TODO: insert return statement here
+	return this->hp;
+}
+
+const int& Character::getHpMax() const
+{
+	// TODO: insert return statement here
+	return this->hpMax;
+}
+
+void Character::setPos(const sf::Vector2f pos)
+{
+	this->sprite.setPosition(pos);
+}
+
+void Character::setPos(const float x, const float y)
+{
+	this->sprite.setPosition(x, y);
+}
+
+void Character::buffHp(const int value)
+{
+	this->hp += value;
+}
+
+void Character::setHp(const int hp)
+{
+	this->hp = hp;
+}
+
+void Character::loseHp(const int value)
+{
+	this->hp -= value;
+	if (this->hp < 0) this->hp = 0;
+}
+
+void Character::update()
+{
+
+}
+
+void Character::render(sf::RenderTarget& target)
+{
+	target.draw(this->sprite);
+}
+
+void Character::move(const float dirX, const float dirY)
+{
+	this->sprite.move(this->moveSpeed * dirX, this->moveSpeed * dirY);
+}
+
+Character::Character()
+{
+	this->hpMax = 400;
+	this->hp = this->hpMax;
+	this->moveSpeed = 3.f;
+	this->initTexture();
+	this->initSprite();
+}
