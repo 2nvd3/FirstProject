712e45f5fd8784474a17602129a54fc5ab958e75
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-12 01:15:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+///  Created by DUNG NGUYENX
+///  R I O   B R O T H E R S
+///  Version 1: 12/04/2023
+///Main
 #include "Header/Utils.h"
 #include <ctime>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	srand(time(0));
-  
+	srand(time(0));//reset random time
+	
 	Game MyGame;
 	MyGame.run();
-  
-	return 0;
 }
