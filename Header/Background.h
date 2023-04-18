261f359bea870e7e42cee73b0e2ef919873d55c3
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-18 15:59:08 +0700

    Create Background.h

---
+++ Header/Background.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include <vector>
+
+class Background
+{
+private:
+	sf::RenderWindow* window;
+
+	class Layer
+	{
+	public:
+		sf::Sprite sprite_array[2];
+
+		float speed;
+		float layer_pos_x[2];
+	};
+
+	std::vector<Layer> Layers;
+	int layer_width;
+
+public:
+	void SetupBackground(sf::RenderWindow *window,std::vector<sf::Texture> &Textures,int layer_width);
+	void MoveandDisplay();
+};
+
+std::vector<sf::Texture> TextureLayers(int typeOfBackground);
