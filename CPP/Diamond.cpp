99623b942990c264e2f9f56561b0a5a6602aa5d9
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-18 15:47:44 +0700

    Update Diamond.cpp

--- CPP/Diamond.cpp
+++ CPP/Diamond.cpp
@@ -9,7 +9,7 @@ void Diamond::initVari()
 {
 	this->PointCount = rand() % 100 + 25;
 	this->type = 0;
-	this->speed = rand() % 5 + 1;
+	this->speed = rand() % 5 + 3;
 	this->hpMax = static_cast<float>(this->PointCount);
 	this->hp = this->hpMax;
 	this->damage = this->PointCount;
@@ -31,14 +31,13 @@ void Diamond::update()
 	this->diamondSprite.move(-this->speed, 0);
 }
 
-void Diamond::render(sf::RenderTarget* target)
+void Diamond::render(sf::RenderTarget *target)
 {
 	target->draw(this->diamondSprite);
 }
 
 const int& Diamond::getPoints() const
 {
-	// TODO: insert return statement here
 	return this->points;
 }
 
