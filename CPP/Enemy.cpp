8b1f5af4ecceb455edba615de2e95979ba1a2da6
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-18 15:48:18 +0700

    Update Enemy.cpp

--- CPP/Enemy.cpp
+++ CPP/Enemy.cpp
@@ -9,7 +9,7 @@ void Enemy::initVari()
 {
 	this->PointCount = rand() % 100 + 25;
 	this->type = 0;
-	this->speed = rand()%5+1;
+	this->speed = rand()%6+2.7;
 	this->hpMax = static_cast<float>(this->PointCount);
 	this->hp = this->hpMax;
 	this->damage = this->PointCount;
@@ -23,13 +23,11 @@ void Enemy::initSprite()
 
 const int& Enemy::getDamage() const
 {
-	// TODO: insert return statement here
 	return this->damage;
 }
 
 const int& Enemy::getPoints() const
 {
-	// TODO: insert return statement here
 	return this->points;
 }
 
@@ -43,9 +41,9 @@ void Enemy::update()
 	this->enemySprite.move(-this->speed, 0);
 }
 
-void Enemy::render(sf::RenderTarget* target)
+void Enemy::render(sf::RenderTarget& target)
 {
-	target->draw(this->enemySprite);
+	target.draw(this->enemySprite);
 }
 
 Enemy::Enemy(float pos_x, float pos_y)
