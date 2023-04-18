e9ef049d23a8ea7e83643645962d682b513f4bde
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-18 20:29:58 +0700

    Update Character.h

--- Header/Character.h
+++ Header/Character.h
@@ -98,3 +98,33 @@ public:
 	void update();
 	void render(sf::RenderTarget* target);
 };
+
+class Diamond
+{
+private:
+	sf::Texture diamondTex;
+	sf::Sprite diamondSprite;
+
+	int type;
+	int hp;
+	int hpMax;
+	int damage;
+	int points;
+	unsigned PointCount;
+	void initTex();
+	void initSprite();
+	void initVari();
+
+public:
+	float speed;
+	float posX;
+	float posY;
+
+	Diamond(float pos_x, float pos_y);
+
+	const sf::FloatRect getBounds() const;
+	const int& getPoints() const;
+
+	void update();
+	void render(sf::RenderTarget* target);
+};
