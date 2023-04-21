0c14b09683881595d87f30c10d39f24e480ed97f
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-21 21:25:59 +0700

    Create main.cpp

---
+++ RioFly 1.0/folder+main/main.cpp
@@ -0,0 +1,14 @@
+///  Created by DUNG NGUYENX
+///  R I O F L Y
+///  Version 1: 12/04/2023
+///Main
+#include "Header/Utils.h"
+#include <ctime>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	srand(time(0));//reset random time
+	
+	Game MyGame;
+	MyGame.run();
+}
