68d4e572a6ef4ef8dea504e08f2829f53138aa16
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-04-21 21:28:24 +0700

    Create Character.h

---
+++ RioFly 1.0/folder+main/Header/Character.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
+class Character
+{
+private:
+	sf::Texture texture;
+	sf::Sprite sprite;
+
+	float moveSpeed;
+
+	int hp;
+	int hpMax;
+  //private function
+	void initTexture();
+	void initSprite();
+public:
+	Character();
+	const sf::FloatRect getBounds() const;
+
+	const int& getHp() const;
+	const int& getHpMax() const;
+
+	void setPos(const sf::Vector2f pos);
+	void setPos(const float x, const float y);
+
+	void buffHp(const int value);
+	void setHp(const int hp);
+	void loseHp(const int value);
+
+	void move(const float dirX,const float dirY);
+	void update();
+	void render(sf::RenderTarget &target);
+};
