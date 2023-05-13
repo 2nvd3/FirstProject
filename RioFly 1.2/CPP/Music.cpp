b574c41e038cdb7f440d50ede5f06ddd7d382273
2nvd3 <127430119+2nvd3@users.noreply.github.com>
2023-05-13 11:49:43 +0700

    Update Music.cpp

--- RioFly 1.2/CPP/Music.cpp
+++ RioFly 1.2/CPP/Music.cpp
@@ -1,1 +1,16 @@
 
+#include "../Header/Music.h"
+
+std::vector<std::string> SetMusics()
+{
+	std::vector<std::string> Songname;
+	for (int i = 0; i < 10; i++)
+	{
+        std::string temp_Name = "Audio/Song_";
+        std::string num_String = std::to_string(i);
+
+        temp_Name += num_String + ".ogg";
+        Songname.push_back(temp_Name);
+    }
+    return Songname;
+}
